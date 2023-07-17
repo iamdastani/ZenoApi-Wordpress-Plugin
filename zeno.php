9d392001bd09deccf17759755af6a23f3fff8b71
iamdastani <iamdastani@gmail.com>
2023-07-17 14:09:34 +0300

    Plugin Info

---
+++ zeno.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: ZenoApi
+Plugin URI: https://zeno.co.tz
+Description: Handles custom registration functionality for Zeno Limited.
+Version: 1.0
+Author: Dastani Ferdinandi
+Author URI: https://instagram.com/iamdastani
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: zeno-login-plugin
+Domain Path: /languages
+*/
